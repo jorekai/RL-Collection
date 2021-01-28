428ce7db31af9d9ed044beb819045086aa216109
jorekai <nils.jorek@gmx.de>
2021-01-28 19:22:30 +0100

    Init agent

--- dqn/agent.py
+++ dqn/agent.py
@@ -1,18 +1,33 @@
+import numpy as np
 
 
 class DQNAgent:
     """
     The DQN Agent
     """
-    def __init__(self, memory, net):
+
+    def __init__(self, env, memory, net, epsilon_init: float = 1):
         """
         The initialization method of our agent
+        :param env: gym environment
         :param memory: replay memory of any size
         :param net: neural network model of any size
+        :param epsilon_init: float value for exploration initialisation
         """
+        self.env = env
         self.memory = memory
         self.net = net
+        self.epsilon = epsilon_init
 
-    def act(self):
-        # implement the action method
-        pass
+    def act(self, state):
+        """
+        Choose an action according to our epsilon-greedy policy
+        :param state: the state observation of our environment we need to predict from
+        :return: action choice
+        """
+        if np.random.random() <= self.epsilon:
+            return self.env.action_space.sample()
+        return np.argmax(self.net.predict(state))
+
+    def replay(self, batch_size: int):
+        
