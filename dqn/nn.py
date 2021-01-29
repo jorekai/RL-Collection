9cc9ee46aa469901f36f67eaa14db5236fdaa32f
jorekai <nils.jorek@gmx.de>
2021-01-29 10:42:23 +0100

    NN

---
+++ dqn/nn.py
@@ -0,0 +1,40 @@
+from tensorflow.keras import Sequential, Dense
+from tensorflow.python.keras.optimizer_v1 import adam
+
+
+class NN:
+    def __init__(self, alpha: float, decay: float):
+        self.alpha = alpha
+        self.decay = decay
+        self.model = self.init_model()
+
+    def init_model(self):
+        """
+        Initializing our keras sequential model
+        :return: initialized model
+        """
+        model = Sequential()
+        model.add(Dense(24, input_dim=4, activation='relu'))
+        model.add(Dense(48, activation='relu'))
+        model.add(Dense(96, activation='relu'))
+        model.add(Dense(48, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(2, activation='relu'))
+        model.compile(loss='mse', optimizer=adam(lr=self.alpha, decay=self.decay))
+        return model
+
+    def predict(self, *args, **kwargs):
+        """
+        By wrapping the keras predict method we can handle our net as a standalone object
+        :param args: interface to keras.model.predict
+        :return: prediction
+        """
+        return self.model.predict(*args, **kwargs)
+
+    def fit(self, *args, **kwargs):
+        """
+        By wrapping the keras fit method we can handle our net as a standalone object
+        :param args: interface to keras.model.fit
+        :return: history object
+        """
+        return self.model.fit(*args, **kwargs)
